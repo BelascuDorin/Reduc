d446ac601e3812fe38236a758c1e93d619028a8b
Belascu Dorin <dorin.belasc@zipperstudios.co>
2021-06-24 15:08:35 +0300

    Added more products statically

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Home from "./home/Home";
 
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState([
+  const [products] = useState([
     {
-      id: 3,
+      id: 1,
       name: "Castraveti",
       category: "",
       shop: "",
       position_on_the_map: {},
+    },
+    {
+      id: 2,
+      name: "rosii",
+      category: "legume",
+      shop: "Profi",
+      position_on_the_map: {
+        latitude: 14.2,
+        longitude: 13.3,
+      },
+    },
+    {
+      id: 3,
+      name: "porumb",
+      category: "legume",
+      shop: "Kaufland",
+      position_on_the_map: {
+        latitude: 12.24,
+        longitude: 15.45,
+      },
     },
   ]);
 
